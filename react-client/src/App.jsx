e6f66a4d2a9e3fb7d6e9c2bbf750442441b73dd2
Corban Joyce <corbantjoyce@gmail.com>
2021-02-15 17:34:09 -0500

    displays users on client side with css styling

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { Users } from './index.js';
 
-function App() {
+function App(client) {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Hello World!!</h1>
+        <Users />
+      </div>
+    </ApolloProvider>
   )
 };
 
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, gql, useQuery } from '@apollo/client';
+import styles from './styles.module.css';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000',
     cache: new InMemoryCache(),
 });
 
-client
-    .query({
-        query: gql`
-            query users {
-                users {
-                id
-                name
-                email
-                phone
-                }
-            }
-        `
-    })
-    .then((result) => console.log(result));
+const USERS = gql`
+    query users {
+        users {
+            id
+            name
+            email
+            phone
+        }
+    }
+`;
+
+function Users() {
+    const { loading, error, data } = useQuery(USERS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    return data.users.map((user, index) => (
+        <div key={user.id}>
+            <p className={styles.name}>
+                {user.name}
+            </p>
+            <p className={styles.email}>
+                {user.email}
+            </p>
+            <p className={styles.phone}>
+                {user.phone}
+            </p>
+        </div>
+    ));
+}
+
+function App() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <h1>Users</h1>
+          <Users />
+        </div>
+      </ApolloProvider>
+    )
+  };
 
 ReactDOM.render(<App />, document.getElementById('root'));
