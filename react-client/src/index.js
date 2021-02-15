b0c69f438152683b3cd0a9d1a8ab2a11d6623240
Corban Joyce <corbantjoyce@gmail.com>
2021-02-15 15:04:13 -0500

    imported apollo client

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloClient, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
