18eaf51795e1cb79e2c82c0b2c61ed5443038f60
Corban Joyce <corbantjoyce@gmail.com>
2021-02-15 15:31:30 -0500

    queries client successfully

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloClient, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
 });
 
-
+client
+    .query({
+        query: gql`
+            query users {
+                users {
+                id
+                name
+                email
+                phone
+                }
+            }
+        `
+    })
+    .then((result) => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
